f200fbbfebf5742795e69f2149196d202d3c11ab
balor001 <balor001@edu.xamk.fi>
2019-11-01 14:21:09 +0200

    Initial Commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Vector3 offset;
+    public Vector3 rotate;
+    public GameObject FollowedObject;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the FollowedObjects position
+        // Set this transform's position
+        this.transform.position = FollowedObject.transform.position;
+        this.transform.rotation = FollowedObject.transform.rotation;
+
+        // Add the offset
+        this.transform.Translate(offset);
+        this.transform.Rotate(rotate);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotatingSpeed;
+
+    private float Vertical;
+    private float Horizontal;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Vertical = Input.GetAxis("Vertical");
+        this.Horizontal = Input.GetAxis("Horizontal");
+
+        this.transform.Rotate(new Vector3(0, Horizontal * Time.deltaTime * speed, 0));
+
+        this.transform.Translate(transform.forward * speed * Vertical * Time.deltaTime);
+    }
+}
